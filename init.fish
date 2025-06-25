97f2f9ef494df2c0c5392dfff7556b870dbd921c
Benign X <1341398182@qq.com>
2025-06-25 11:05:21 +0800

    update init script
    Signed-off-by: Benign X <1341398182@qq.com>

--- init.fish
+++ init.fish
@@ -5,8 +5,30 @@ pushd ops
 . ./change_shell.fish
 . ./install_packages.fish
 
-install_packages \
-        orbstack \
-        pyenv
+
+
+# Common packages
+set common_packages pyenv curl
+
+# Platform-specific packages
+set macos_packages orbstack yadm
+set linux_packages build-essential neofetch
+
+# Install common packages
+echo "[INFO] Installing common packages..."
+install_packages $common_packages
+
+# Install platform-specific packages
+switch "$OS_TYPE"
+    case "macos"
+        echo "[INFO] Installing macOS-specific packages..."
+        install_packages $macos_packages
+    case "arch" "ubuntu" "debian"
+        echo "[INFO] Installing Linux-specific packages..."
+        install_packages $linux_packages
+    case '*'
+        echo "[WARN] Package install not supported on OS_TYPE=$OS_TYPE"
+        return 1
+end
 
 popd
